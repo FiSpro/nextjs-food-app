ce143ac091083983f2b56aec334486838b728050
FiSpro <filipspro@gmail.com>
2024-12-05 21:26:44 +0100

    Saving new meals to database and displaying them

--- app/meals/share/actions.js
+++ app/meals/share/actions.js
@@ -2,13 +2,12 @@
 
 import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
-import { revalidatePath } from "next/cache";
 
 function isInvalidText(text) {
   return !text || text.trim() === "";
 }
 
-export async function shareMeal(prevState, formData) {
+export async function shareMeal(formData) {
   const meal = {
     title: formData.get("title"),
     summary: formData.get("summary"),
@@ -28,12 +27,9 @@ export async function shareMeal(prevState, formData) {
     !meal.image ||
     meal.image.size === 0
   ) {
-    return {
-      message: "Invalid input.",
-    };
+    throw new Error("Invalid input");
   }
 
   await saveMeal(meal);
-  revalidatePath("/meals");
   redirect("/meals");
 }
